3d0713169b4c9b451dc7d266eaada8f24714c1ac
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2024-12-19 15:01:49 +0530

    Add Tab and Tabs components; create layout for tabs view with initial template

---
+++ resources/views/layouts/tabs.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Tabs</title>
+</head>
+<body>
+<div id="app" class="p-8">
+    <h1 class="text-2xl font-bold mb-6">Tabs</h1>
+
+    <tabs>
+        <tab title="first">
+            <p>Hello</p>
+        </tab>
+        <tab title="second">
+            <p>World</p>
+        </tab>
+        <tab title="third">
+            <p>!!!</p>
+        </tab>
+    </tabs>
+
+</div>
+
+</body>
+</html>
