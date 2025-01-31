13178620dd089ad6eabc911d4b06ad95cf6bcc21
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2025-01-31 10:19:56 +0530

    Add GlobalStore utility for managing loading state; implement methods to add, remove, and check loading requests

---
+++ resources/js/utilities/GlobalStore.js
@@ -0,0 +1,18 @@
+export default class GlobalStore {
+    static loadingStack = [];
+
+    static addLoadingRequest(requestId) {
+        this.loadingStack.push(requestId);
+    }
+
+    static removeLoadingRequest(requestId) {
+        const index = this.loadingStack.indexOf(requestId);
+        if (index !== -1) {
+            this.loadingStack.splice(index, 1);
+        }
+    }
+
+    static isLoading() {
+        return this.loadingStack.length > 0;
+    }
+}
