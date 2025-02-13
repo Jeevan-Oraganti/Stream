d285aa6b624db226dbc61c2c35e9ab2f3e6be0b3
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2025-02-13 16:58:48 +0530

    Enhance pagination component styling for improved button alignment and display

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="mt-4 flex justify-center items-center space-x-2">
         <button v-if="pagination.prev_page_url" @click="$emit('paginate', pagination.prev_page_url)"
-            class="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition">
+            class="px-4 py-2 bg-gray-300 text-gray-700 rounded-l-md hover:bg-gray-400 transition">
             Previous
         </button>
-        <span class="px-4 py-2 bg-blue-500 text-white rounded-md">
+        <span class="px-4 py-2 bg-white text-gray-700 border-t border-b">
             Page {{ pagination.current_page }} of {{ pagination.last_page }}
         </span>
         <button v-if="pagination.next_page_url" @click="$emit('paginate', pagination.next_page_url)"
-            class="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition">
+            class="px-4 py-2 bg-gray-300 text-gray-700 rounded-r-md hover:bg-gray-400 transition">
             Next
         </button>
     </div>
