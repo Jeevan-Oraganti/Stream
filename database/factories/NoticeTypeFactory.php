119d0c693054feff4d9a8f9fcb7e4681a4f97d56
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2025-03-28 10:30:08 +0530

    Refactor test suite: remove obsolete tests, add new NoticeTest, and configure testing environment

---
+++ database/factories/NoticeTypeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\NoticeType>
+ */
+class NoticeTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'type' => $this->faker->word, // Generate a random word for the type
+            'color' => $this->faker->hexColor, // Generate a random hex color
+        ];
+    }
+}
