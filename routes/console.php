847ee54a1ff646edba9d9cb298f508ba58ea6789
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2025-03-28 10:30:28 +0530

    Update testing environment configuration and add recurring notice scheduling command

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,13 @@ use App\Models\Notice;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use app\Console\Commands\ScheduledNotices;
+use app\Console\Commands\RecurringNotices;
 use Illuminate\Support\Facades\Schedule;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('notices:scheduled-notices')->everyMinute();
+Schedule::command('notices:schedule-recurring')->everyMinute();
