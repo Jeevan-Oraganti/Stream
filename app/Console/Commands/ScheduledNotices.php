e5392b936089021c90df4a9f59df4774c775ee2e
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2025-04-07 11:28:24 +0530

    Refactor ScheduledNotices command: streamline notice activation logic and enhance query readability

--- app/Console/Commands/ScheduledNotices.php
+++ app/Console/Commands/ScheduledNotices.php
@@ -26,11 +26,6 @@ class ScheduledNotices extends Command
      * Execute the console command.
      */
 
-//     start DB transaction
-//     deactivate all notices which are active
-//     activate selected notices with the below query
-//     commit DB transaction
-
     public function handle()
     {
         DB::beginTransaction();
@@ -40,31 +35,38 @@ class ScheduledNotices extends Command
             Log::info('All active notices have been deactivated.');
 
             // Fetch notices to activate
-            $noticesToActivate = DB::select("
-            SELECT n.*,
-               (SELECT COUNT(mc.calendar_date)
-                FROM market_calendars mc
-                WHERE mc.calendar_date::DATE BETWEEN NOW()::DATE AND n.event_date::DATE
-            AND mc.is_holiday IS FALSE
-            AND mc.calendar_date::DATE != NOW()::DATE) AS days_before_active
-            FROM notices n
-            WHERE n.is_active = false
-            AND (n.expiry_date > NOW() OR n.expiry_date IS NULL)
-            ORDER BY n.event_date ASC
+            $noticesToActivate = Notice::fromQuery("
+            SELECT * FROM
+                         (SELECT n.*,
+                                 (SELECT COUNT(mc.calendar_date)
+                                  FROM market_calendars mc
+                                  WHERE mc.calendar_date :: DATE BETWEEN NOW() :: DATE
+                                      AND n.event_date :: DATE
+                                    AND mc.is_holiday IS FALSE
+                                    AND mc.calendar_date :: DATE != NOW() :: DATE) AS days_before_active
+                        FROM notices n
+                        WHERE n.is_active = false
+                          AND (n.expiry_date > NOW() OR n.expiry_date IS NULL)
+                        ORDER BY n.event_date) AS SUB_QUERY
+                     WHERE (sub_query.notice_type_id = 4 AND SUB_QUERY.days_before_active = 1)
+                        OR (sub_query.notice_type_id != 4)
         ");
 
-        // Activate selected notices
-        foreach ($noticesToActivate as $notice) {
-            $threshold = 1;
-            if ($notice->days_before_active <= $threshold) {
-                DB::table('notices')->where('id', $notice->id)->update(['is_active' => true]);
-                Log::info("Notice ID {$notice->id} is now active.");
-            }
-        }
 
-        DB::commit();
-        Log::info('Scheduled notices processed successfully.');
-    } catch (\Exception $e) {
+            // Activate selected notices
+            foreach ($noticesToActivate as $notice) {
+                $threshold = 1;
+                if ($notice->days_before_active <= $threshold) {
+                    $notice->is_active = true;
+                    $notice->update();
+                    //       DB::table('notices')->where('id', $notice->id)->update(['is_active' => true]);
+                    Log::info("Notice ID {$notice->id} is now active.");
+                }
+            }
+
+            DB::commit();
+            Log::info('Scheduled notices processed successfully.');
+        } catch (\Exception $e) {
             DB::rollBack();
             Log::error('Error processing scheduled notices: ' . $e->getMessage());
         }
